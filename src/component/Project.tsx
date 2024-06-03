80f011a3c9d9a8353d21f4008a41f9df17ba19b7
shubham agrahari <aakashagrahari26@gmail.com>
2024-06-04 00:10:26 +0530

    added video player component

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -1,6 +1,15 @@
 import { PROJECTS } from "./constant";
+import { ProjectDetail } from "./project/ProjectDetail";
 const Project = () => {
-  return <div className="borderBottom py-10">Project</div>;
+  return (
+    <div className="borderBottom py-10">
+      {PROJECTS?.map((project, index) => (
+        <div key={`${index}-${project.projectName}`}>
+          <ProjectDetail {...project} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Project;
--- src/component/project/ShortVideoPlayer.tsx
+++ src/component/project/ShortVideoPlayer.tsx
@@ -1,8 +1,15 @@
-const ShortVideoPlayer = () => {
+export type shortVideoplayerType = {
+  imageUrl: string;
+  videoUrl: string;
+};
+const ShortVideoPlayer = (props: shortVideoplayerType) => {
+  const { imageUrl, videoUrl } = props;
+
   return (
-    <section>
-      <h1></h1>
-    </section>
+    <div className="relative border-2 border-red-700">
+      <img className="absolute" src={imageUrl} alt="" />
+      <video src={videoUrl} loop muted controls />
+    </div>
   );
 };
 
