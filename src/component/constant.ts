71b1708186dc42487f0bc39fa34f412c09140602
shubham agrahari <aakashagrahari26@gmail.com>
2024-06-15 18:14:38 +0530

    added mapty description and video

--- src/component/constant.ts
+++ src/component/constant.ts
@@ -1,6 +1,8 @@
 import indexnine_logo from "../assets/indexnine_technologies_logo.jpeg";
 import TicTacImageUrl from "../assets/TictacImg.png";
 import TicTacvideoUrl from "../assets/ticTacV.mp4";
+import MaptyVideo from "../assets/MaptyVideo.mp4";
+import MaptyImg from "../assets/MaptyImg.png";
 
 export const ABOUTME =
   "Software Engineer with 1 year of experience specializing in front-end development using React and TypeScript. Proficient in state management with Redux Toolkit, unit testing with React Testing Library, and component testing with Storybook. Skilled in JavaScript and experienced in data visualization using ApexCharts. Passionate about creating efficient and maintainable code.";
@@ -61,16 +63,21 @@ export const PROJECTS = [
     imageUrl: TicTacImageUrl,
   },
   {
-    projectName: "Tic Tac Toe",
+    projectName: "Mapty",
     projectSummary: `
-      The Tic Tac Toe Game project involves designing and developing a classic 
-      two-player game where players take turns marking the spaces in a 3x3 grid.
-      The objective is to place three respective marks in a horizontal,
-      vertical, or diagonal row to win the game. This project will help
-      you understand the basics of game development, user interface design,
-      and implementing game logic`,
-    websiteUrl: " https://shubham12346.github.io/tic-tac.github.io/",
-    videoUrl: TicTacvideoUrl,
-    imageUrl: TicTacImageUrl,
+    I developed an Interactive Workout Tracker using HTML, CSS, JavaScript,
+     and Object-Oriented Programming (OOP) to provide users with a comprehensive 
+     platform to log and visualize their workouts on an interactive map. 
+     The application allows users to create and display their workouts, 
+     categorized by type (Cycling in yellow and Running in green), with
+    each workout displayed on the map. Users can easily navigate through a
+     sidebar workout list, and selecting a workout zooms the map to the 
+     corresponding location, offering a detailed view. The project 
+    emphasizes modularity and reusability through OOP, ensuring 
+    a maintainable and scalable codebase.
+      `,
+    websiteUrl: " https://shubham12346.github.io/Mapty.github.io/",
+    videoUrl: MaptyVideo,
+    imageUrl: MaptyImg,
   },
 ];
